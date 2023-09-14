e5232d5e3765de51022f6e300c3829267ac784d8
Rasel <USER@DESKTOP-LPSJO96>
2023-09-14 17:33:45 +0600

    asp.net core mvc with .net 6 all controller and action method done

---
+++ PracticeCoreMVC/Models/EmployeeValidator.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace PracticeCoreMVC.Models
+{
+    public class EmployeeValidator
+    {
+        [Required]
+        [MaxLength(50)]
+        [Display(Name = "Employee Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Employee Salary")]
+        public decimal Salary { get; set; }
+
+        [Required]
+        [Display(Name = "Joining Date")]
+        public decimal JoiningDate { get; set; }
+    }
+
+    [ModelMetadataType(typeof(EmployeeValidator))]
+    public partial class Employee
+    {
+    }
+}
--- PracticeCoreMVC/Program.cs
+++ PracticeCoreMVC/Program.cs
@@ -27,6 +27,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Employee}/{action=Index}/{id?}");
 
 app.Run();
